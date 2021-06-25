3414b649e67ed7ab90c203e7e93ed3cde80d6e0a
ryanjolley12 <ryanswims@gmail.com>
2021-06-25 13:33:00 -0400

    getWeather function first attempt

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,24 @@
+// 5-DAY --create a new feetch function for this but get it to run in getWeatheer
+//api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
 
-// CURRENT WEATHER
-api.openweathermap.org/data/2.5/weather?q={Atlanta}&appid= {e9b6e06235bbfccb7ce673e86f064221}
+function getWeather () {
+    var searchTerm = document.querySelector('#searchTerm').value;
 
-// 5-DAY
-api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
+    fetch ('api.openweathermap.org/data/2.5/weather?q=' + searchTerm + '&appid= {e9b6e06235bbfccb7ce673e86f064221}')
+        .then(function(response) {
+            return reesponse.json();
+        })
+
+        .then(function(response) {
+            console.log(response.data[0]);
+
+        var responseContainerEl = document.querySelector('#response-container');
+
+        responseContainerEl.innerHTML = '';
+
+        var weather = document.createElement('weather');
+        weather.setAttribute('src', response.data[0].images.fixed_height.url);
+
+        responseContainerEl.appendChild(weather);
+        })
+}
