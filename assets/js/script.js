48efa0d6da6f29b0ba16971b27b7618045f185ea
ryanjolley12 <ryanswims@gmail.com>
2021-06-25 12:39:37 -0400

    bootstrap and api keys

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+
+// CURRENT WEATHER
+api.openweathermap.org/data/2.5/weather?q={Atlanta}&appid= {e9b6e06235bbfccb7ce673e86f064221}
+
+// 5-DAY
+api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
